89f41cc7b0d35e90b16208e961f12398dcb378a1
unknown <rohit121098@gmail.com>
2020-10-10 13:09:38 +0530

    ch(app): Add application Files

--- nearbyapis/settings.py
+++ nearbyapis/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ('SECRET_KEY')
+SECRET_KEY = 'z8jlv!$x7tk!9eg76l#310&uusb0=vxj=_oh$v=(egt1rc-!xu'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
